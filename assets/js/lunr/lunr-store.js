68b9e211c99983d4cf770ee1e8b04df97702dab5
wildone <wildone@users.noreply.github.com>
2020-02-10 01:24:57 +0000

    Automated deployment: Mon Feb 10 01:24:57 UTC 2020 c3edbb3e8868f5995965e13467c2f44be2a4782f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -185,7 +185,7 @@ var store = [{
         "url": "https://aem.design/blog/2020/01/12/aem-framework-for-multi-tenant-environments",
         "teaser":null},{
         "title": "AEM SaaS Hands On 😍🚀🔭",
-        "excerpt":"On This Page Introduction AEM SaaS Author Hand On Environments Pipeline AEM SaaS Pipelines Breakdown Validation Build &amp; Unit Testing Code Scanning Build Images Deploy to Dev AEM SaaS Pipelines Whats missing Git Peer Review Workflow Automated Functional testing No Git LFS No Felix Console Conclusion Introduction If Github Actions...","categories": ["blog"],
+        "excerpt":"On This Page Introduction AEM SaaS Author Hand On Environments Pipeline AEM SaaS Pipelines Breakdown Validation Build &amp; Unit Testing Code Scanning Build Images Deploy to Dev AEM SaaS Pipelines What’s missing Git Peer Review Workflow Automated Functional testing No Git LFS No Felix Console Conclusion Introduction If Github Actions...","categories": ["blog"],
         "tags": ["devops","aem","saas","knowledge","sharing","handson"],
         "url": "https://aem.design/blog/2020/02/01/aem-saas-hands-on%F0%9F%98%8D%F0%9F%9A%80%F0%9F%94%AD",
         "teaser":null}]
