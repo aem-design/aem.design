0b1d33d5bda7fff234e36f2b6d60db068b54a14a
saketkrnph <saketkrnph@users.noreply.github.com>
2020-03-15 13:23:50 +0000

    Automated deployment: Sun Mar 15 13:23:49 UTC 2020 41e410b9a7ad22ba9e1895652d9032f42ccd2446

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -250,4 +250,10 @@ var store = [{
         "tags": ["devops","aem","saas","knowledge","sharing","handson"],
         "url": "https://aem.design/blog/2020/02/01/aem-saas-hands-on%F0%9F%98%8D%F0%9F%9A%80%F0%9F%94%AD",
         "teaser": null
+      },{
+        "title": "Aem Magento Integration",
+        "excerpt":"AEM with Magento Integration Introduction AEM when integrated with an eCommerce provider provides a great experience for online shopping functionalities. eCommerce functionality is inherent in AEM which shows from demo sites like we retail. As AEM provides very perfect handling of content like authoring and publishing and managing assets, integrating...","categories": [],
+        "tags": [],
+        "url": "https://aem.design/aem-magento-integration/",
+        "teaser": null
       }]
