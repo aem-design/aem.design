7fd236818aabb3f4b3db50e8fd5ca8c0ccba8947
wildone <max.barrass@gmail.com>
2021-03-13 12:54:26 +1100

    update scripts

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem "ffi", ">= 1.9.24"
 gem "i18n", ">= 1.8.2"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
-gem "minimal-mistakes-jekyll", ">= 4.20.1"
+gem "minimal-mistakes-jekyll", ">= 4.22.0"
 
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
@@ -31,4 +31,7 @@ group :jekyll_plugins do
    gem 'jekyll-sitemap'
    gem 'jekyll-paginate-v2'
    gem 'jekyll-include-cache'
+   gem 'jekyll-last-modified-at'
+   gem 'jekyll-algolia'
+   gem 'jekyll-archives'
 end
--- serve.ps1
+++ serve.ps1
@@ -3,12 +3,17 @@ $SITE_NAME = Get-Content .\CNAME -Raw
 $SITE_NAME = $SITE_NAME -replace "[.\n\r]", "-"
 $CONATINER_NAME = "build-$SITE_NAME"
 
+$PORT = Get-Content .\dev.port -Raw
+$PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
+
 $DOCKER_INSPECT_CHECK=$( docker inspect --format "{{.State.Status}}" $CONATINER_NAME )
+
+Start-Process 'http://localhost:8105/'
 
 if ($DOCKER_INSPECT_CHECK -contains "running") {
   Write-Host "Shell into existing Docker container:"
   docker exec -it $CONATINER_NAME bash
 } else {
   Write-Host "Starting Docker container to run build server:"
-  docker run --name $CONATINER_NAME -it --rm -p 8103:8100 -p 35730:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+  docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
 }
