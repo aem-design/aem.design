c478cabe009167a0a657ae5955e49ba57cec37c2
wildone <max.barrass@gmail.com>
2021-03-14 01:51:03 +1100

    add trace

--- docker-serve.sh
+++ docker-serve.sh
@@ -12,4 +12,4 @@ rvm install ruby --latest
 gem install i18n jekyll bundler jemoji nokogiri -n /usr/local/bin
 
 bundle install
-bundle exec jekyll serve --host 0.0.0.0 --port "${PORT}" --livereload --livereload_port "${PORT_LIVERELOAD}" --force_polling --future
+bundle exec jekyll serve --trace --host 0.0.0.0 --port "${PORT}" --livereload --livereload_port "${PORT_LIVERELOAD}" --force_polling --future
