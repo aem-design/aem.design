bfe01da5f4efd646c226bf090cb529aa0582bd5e
wildone <max.barrass@gmail.com>
2021-03-14 01:51:31 +1100

    add bash login flag

--- serve.ps1
+++ serve.ps1
@@ -12,7 +12,7 @@ Start-Process 'http://localhost:8105/'
 
 if ($DOCKER_INSPECT_CHECK -contains "running") {
   Write-Host "Shell into existing Docker container:"
-  docker exec -it $CONATINER_NAME bash
+  docker exec -it $CONATINER_NAME bash --login
 } else {
   Write-Host "Starting Docker container to run build server:"
   docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
