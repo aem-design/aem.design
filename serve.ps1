9f294c88297098c27d0c1c5fe5b97c669a79344f
wildone <max.barrass@gmail.com>
2021-03-14 19:50:49 +1100

    update scripts [skip ci]

--- serve.ps1
+++ serve.ps1
@@ -6,14 +6,5 @@ $CONATINER_NAME = "build-$SITE_NAME"
 $PORT = Get-Content .\dev.port -Raw
 $PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
 
-$DOCKER_INSPECT_CHECK=$( docker inspect --format "{{.State.Status}}" $CONATINER_NAME )
-
-Start-Process 'http://localhost:8105/'
-
-if ($DOCKER_INSPECT_CHECK -contains "running") {
-  Write-Host "Shell into existing Docker container:"
-  docker exec -it $CONATINER_NAME bash --login
-} else {
-  Write-Host "Starting Docker container to run build server:"
-  docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
-}
+Write-Host "Starting Docker container to run build server:"
+docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
--- serve.sh
+++ serve.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-rm -rf ./_site
-bundle exec jekyll serve
-#bundle exec jekyll serve --ssl-key .ssl/domain-key.txt  --ssl-cert .ssl/domain-crt.txt
+SITE_NAME=$(cat CNAME)
+SITE_NAME=${SITE_NAME//[^a-zA-Z0-9-]/-}
+CONATINER_NAME = "build-$SITE_NAME"
+
+PORT=$(cat dev.port)
+PORT_LIVERELOAD =$(cat dev-livereload.port)
+
+$DOCKER_INSPECT_CHECK=$( docker inspect --format "{{.State.Status}}" $CONATINER_NAME )
+
+Start-Process 'http://localhost:8105/'
+
+if ($DOCKER_INSPECT_CHECK -contains "running") {
+  Write-Host "Shell into existing Docker container:"
+  docker exec -it $CONATINER_NAME bash --login
+} else {
+  Write-Host "Starting Docker container to run build server:"
+  docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+}
