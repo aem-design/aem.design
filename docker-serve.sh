a70041fd9aaae7db2f625be68ac0e72564338720
wildone <wildone@users.noreply.github.com>
2021-03-16 14:12:50 +0000

    deploy: 5231a1de63a138fa370128a47852189356cc64be

--- docker-serve.sh
+++ docker-serve.sh
@@ -2,6 +2,7 @@
 
 cd /build/source
 
+export LANG=en_US.UTF-8
 
 PORT=$(cat dev.port)
 PORT_LIVERELOAD=$(cat dev-livereload.port)
@@ -12,4 +13,4 @@ rvm install ruby --latest
 gem install i18n jekyll bundler jemoji nokogiri -n /usr/local/bin
 
 bundle install
-bundle exec jekyll serve --trace --host 0.0.0.0 --port "${PORT}" --livereload --livereload_port "${PORT_LIVERELOAD}" --force_polling --future
+bundle exec jekyll serve --trace --host 0.0.0.0 --port "${PORT}" --livereload --livereload_port "${PORT_LIVERELOAD}" --force_polling --future --incremental
