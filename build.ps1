f8c3402a501940998a823c03f60fc9765f995fd3
wildone <wildone@users.noreply.github.com>
2021-03-16 12:35:11 +0000

    deploy: 8c94faca3849652a7c3513cb31f7519b65ef096b

--- build.ps1
+++ build.ps1
@@ -1,3 +1,10 @@
-Write-Host "Starting Docker container to run build server"
 
-docker run --name build-si -it --rm -p 4000:4000 -p 35729:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-build.sh
+$SITE_NAME = Get-Content .\CNAME -Raw
+$SITE_NAME = $SITE_NAME -replace "[.\n\r]", "-"
+$CONATINER_NAME = "build-$SITE_NAME"
+
+$PORT = Get-Content .\dev.port -Raw
+$PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
+
+Write-Host "Starting Docker container to run build server:"
+docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-build.sh
--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-bundle exec jekyll build
-bundle exec htmlproofer --check-html \
-        --internal-domains localhost:4000 \
-        --assume-extension \
-        --disable-external \
-        --url-ignore "/#.*/" \
-        _site
+#!/usr/bin/env bash
+
+SITE_NAME=$(cat CNAME)
+SITE_NAME=${SITE_NAME//[^a-zA-Z0-9-]/-}
+CONATINER_NAME = "build-$SITE_NAME"
+
+PORT=$(cat dev.port)
+PORT_LIVERELOAD =$(cat dev-livereload.port)
+
+Write-Host "Starting Docker container to run build server:"
+docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-build.sh
