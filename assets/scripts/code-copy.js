0bc9524b1529ebfecbbb7d4f957ce76b2286c389
wildone <max.barrass@gmail.com>
2021-03-17 01:12:34 +1100

    update copy-code script

--- assets/scripts/code-copy.js
+++ assets/scripts/code-copy.js
@@ -1,40 +1,43 @@
-var codeBlocks = document.querySelectorAll('.code-copy-header + .highlighter-rouge');
-var copyCodeButtons = document.querySelectorAll('.copy-code-button');
+document.addEventListener("DOMContentLoaded", function() {
+  var codeBlocks = document.querySelectorAll('.code-copy-header + .highlighter-rouge');
+  var copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
-// return a promise
-function copyToClipboard(textToCopy) {
-  // navigator clipboard api needs a secure context (https)
-  if (navigator.clipboard && window.isSecureContext) {
-    // navigator clipboard api method'
-    return navigator.clipboard.writeText(textToCopy);
-  } else {
-    // text area method
-    let textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    // make the textarea out of viewport
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    return new Promise((res, rej) => {
-      // here the magic happens
-      document.execCommand('copy') ? res() : rej();
-      textArea.remove();
-    });
+  // return a promise
+  function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+      // navigator clipboard api method'
+      return navigator.clipboard.writeText(textToCopy);
+    } else {
+      // text area method
+      let textArea = document.createElement("textarea");
+      textArea.value = textToCopy;
+      // make the textarea out of viewport
+      textArea.style.position = "fixed";
+      textArea.style.left = "-999999px";
+      textArea.style.top = "-999999px";
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      return new Promise((res, rej) => {
+        // here the magic happens
+        document.execCommand('copy') ? res() : rej();
+        textArea.remove();
+      });
+    }
   }
-}
 
-copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
+  copyCodeButtons.forEach((copyCodeButton, index) => {
+    const code = codeBlocks[index].innerText;
 
-  copyCodeButton.addEventListener('click', () => {
-    copyToClipboard(code);
-    copyCodeButton.classList.add('copied');
+    copyCodeButton.addEventListener('click', () => {
+      copyToClipboard(code);
+      copyCodeButton.classList.add('copied');
 
-    setTimeout(() => {
-      copyCodeButton.classList.remove('copied');
-    }, 2000);
+      setTimeout(() => {
+        copyCodeButton.classList.remove('copied');
+      }, 2000);
+    });
   });
+
 });
