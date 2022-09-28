8c4cecfae871752f5773e9066bbf754a314651be
wildone <max.barrass@gmail.com>
2022-09-28 20:38:54 +1000

    update html_proofer ignore urls.

--- _plugins/050-html-proofer.rb
+++ _plugins/050-html-proofer.rb
@@ -9,10 +9,12 @@ Jekyll::Hooks.register :site, :post_write do |site|
     :assume_extension => ".html",
     :allow_hash_href => true,
     :verbose => true,
-    :internal_domains => ["localhost:8100","localhost:8111"],
+    :internal_domains => ["localhost:8100","localhost:8111", "localhost"],
     :root_dir => "_site",
-    :url_ignore => [
-      /.*#data=.*/,
+    :ignore_urls => [
+      /#data=/,
+      /localhost/,
+      /192.168/,
     ],
     :extension => ".html",
     :log_level => :debug,
