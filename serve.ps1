4119d710a31a930d7a625d357d89c0141d847a5a
wildone <wildone@users.noreply.github.com>
2023-04-05 02:13:10 +0000

    deploy: 5012106424b99797e22681f00f5b0f6bb0b1b24f

--- serve.ps1
+++ serve.ps1
@@ -6,5 +6,8 @@ $CONATINER_NAME = "build-$SITE_NAME"
 $PORT = Get-Content .\dev.port -Raw
 $PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
 
+Write-Host "Remove existing container:"
+docker rm -f $CONATINER_NAME
+
 Write-Host "Starting Docker container to run build server:"
 docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
