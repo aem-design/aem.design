ce40ae1237e8a744201e91dddf939abc65d105c7
wildone <max.barrass@gmail.com>
2023-05-03 04:59:53 +1000

    update helper.

--- serve.ps1
+++ serve.ps1
@@ -10,4 +10,4 @@ Write-Host "Remove existing container:"
 docker rm -f $CONATINER_NAME
 
 Write-Host "Starting Docker container to run build server:"
-docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -e LANG=en_US.UTF-8 -e TZ=Australia/Melbourne  -v ${PWD}:/build/source:rw aemdesign/java-buildpack:ubuntu bash --login /build/source/docker-serve.sh
